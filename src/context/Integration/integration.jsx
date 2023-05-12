0e9aada8acc43d0357c976ecbfbfbe0396cc391a
Logon Aluno <logonrmlocal@fiap.com.br>
2023-05-12 08:11:40 -0300

    adding project

---
+++ src/context/Integration/integration.jsx
@@ -0,0 +1,23 @@
+//axio
+import axios from "axios"
+
+export const GetPaisAxios = (CallBack) => {
+
+    axios({
+        method:'get',
+        url: 'https://servicodados.ibge.gov.br/api/v1/localidades/paises'
+    }).then((resp) => {
+        CallBack(resp.data)
+    })
+
+} 
+
+
+//fetch
+export const GetPaisFetch = async (CallBack) => {
+
+    var url = 'https://servicodados.ibge.gov.br/api/v1/localidades/paises'
+    await fetch(url)
+        .then(resp => resp.json())
+        .then(res => CallBack(res))
+}
---
+++ src/context/Routes/routes.jsx
@@ -0,0 +1,13 @@
+import { Home } from "../Screens/Home"
+import { Icons } from "../Components/Icon/Index"
+
+export const ScreenArray = [
+    {
+        route: 'Home',
+        label: 'Home',
+        typeIcon: Icons.Feather, 
+        icon:'home', 
+        component: Home, 
+        notification: 0
+    }
+]
